0b7180d92f01a9f0e6a0fe7cfe8741c290293d24
Simone Pagliuca <pagliuca.simone01@gmail.com>
2024-10-24 09:20:10 +0200

    Thermal resistances
    Added functions for thermal resistance of different components

--- functions.py
+++ functions.py
@@ -77,3 +77,61 @@ def power_profile_step(h, h_from_c, peak_factors, q_linear_avg):
             return power_values[i]
     return power_values[-1]  # Return the last power value for h = 850
 
+##################################################
+# Radial Temperature Profile
+##################################################
+def thermal_resistance_coolant(radius_cladding_out, coolant_htc):
+    # Calculate the thermal resistance of the coolant
+    thermal_resistance = 1 / (2 * np.pi * radius_cladding_out * coolant_htc)
+    return thermal_resistance
+
+def thermal_resistance_cladding(radius_cladding_out, radius_cladding_in, cladding, temperature):
+    # Calculate the thermal resistance of the cladding
+    k = cladding.Thermal_Conductivity(temperature)
+    thermal_resistance = np.log(radius_cladding_out / radius_cladding_in) / (2 * np.pi * k)
+    return thermal_resistance
+
+def thermal_resistance_gap(effective_size, nominal_size, radius_gap_in, 
+                           helium, fuel, cladding, 
+                           temperature_gas, temperature_fuel_out, temperature_cladding_in,
+                           contact_pressure, cladding_hardness, C,
+                           emissivity_fuel, emissivity_cladding):
+
+    # Three components of the thermal resistance
+    # Conductive heat transfer
+    conduction = helium.Thermal_Conductivity(temperature_gas) / effective_size 
+
+    # Radiative heat transfer
+    radiation = 4 * 5.67e-8 * (temperature_fuel_out**3) / (1/(emissivity_fuel) + 1/(emissivity_cladding) - 1)
+
+    # Contact heat transfer
+    # contact = C * (fuel.Thermal_Conductivity(temperature_fuel_out) * 
+    #                cladding.Thermal_Conductivity(temperature_cladding_in) * 
+    #                contact_pressure) / ((fuel.Thermal_Conductivity(temperature_fuel_out) + 
+    #                cladding.Thermal_Conductivity(temperature_cladding_in))*
+    #                (np.sqrt(nominal_size)*cladding_hardness))
+    
+    # Overall heat transfer coefficient
+    htc = conduction + radiation #+ contact
+
+    # Calculate the thermal resistance of the gap
+    thermal_resistance = 1/(2 * np.pi * radius_gap_in * htc)
+    return thermal_resistance
+
+def thermal_resistance_fuel(Burnup, temperature, Oxigen_to_metal_ratio, Pu_concentration, porosity):
+    # Define constants
+    A = 0.01926 + 1.06e-6 * Oxigen_to_metal_ratio + 2.63e-8 * Pu_concentration
+    B = 2.39e-4 + 1.37e-13 * Pu_concentration
+    D = 5.27e9
+    E = 17109.5
+    beta = 1  # You might need to provide the actual value of beta
+
+    # Calculate k_0
+    k_0 = (1 / (A + B * temperature) + D / (temperature**2) * np.exp(-E / temperature)) * (1 - porosity)**2.5
+
+    # Calculate final thermal conductivity k
+    k = 1.755 + (k_0 - 1.755) * np.exp(-Burnup / 128.75)
+
+    # Calculate the thermal resistance of the fuel
+    thermal_resistance = 1 / (4 * np.pi * k)
+    return thermal_resistance
