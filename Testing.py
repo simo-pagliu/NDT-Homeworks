9a7dc59d55684f4329ac9c804074edb00c946065
Simone Pagliuca <pagliuca.simone01@gmail.com>
2024-10-25 19:06:51 +0200

    refactoring
    temperature profile is broken

--- Testing.py
+++ Testing.py
@@ -1,85 +1,26 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import nuclei_func as nf
-from scipy.interpolate import make_interp_spline
+def thermal_resistance_gap(effective_size, nominal_size, radius_gap_in, 
+                           helium, fuel, cladding, 
+                           temperature_gas, temperature_fuel_out, temperature_cladding_in,
+                           contact_pressure, cladding_hardness, C,
+                           emissivity_fuel, emissivity_cladding):
 
-h_from_c = [42.5, 127.5, 212.5, 297.5, 382.5, 467.5, 552.5, 637.5, 722.5, 807.5]
-peak_factors = [0.572, 0.737, 0.868, 0.958, 1, 0.983, 0.912, 0.802, 0.658, 0.498]
-A = 238
-sigma_scattering = nf.macro(2.5, 10.5, 238)
-sigma_trans = sigma_scattering * (1 + 2 / (3 * A))
-extrapolated_length = 0.714 / sigma_trans
-print(extrapolated_length)
+    # Three components of the thermal resistance
+    # Conductive heat transfer
+    conduction = helium.Thermal_Conductivity(temperature_gas) / effective_size 
 
-def power_profile(x):
-    length_h_from_c = len(h_from_c)
+    # Radiative heat transfer
+    radiation = 4 * 5.67e-8 * (temperature_fuel_out**3) / (1/(emissivity_fuel) + 1/(emissivity_cladding) - 1)
 
-    # Define a vector that returns the start and end points of each interval
-    length_interval = 1 + length_h_from_c  # One extra for the initial 0, and one for the final 850
-    interval = np.zeros(length_interval)
+    # Contact heat transfer
+    # contact = C * (fuel.Thermal_Conductivity(temperature_fuel_out) * 
+    #                cladding.Thermal_Conductivity(temperature_cladding_in) * 
+    #                contact_pressure) / ((fuel.Thermal_Conductivity(temperature_fuel_out) + 
+    #                cladding.Thermal_Conductivity(temperature_cladding_in))*
+    #                (np.sqrt(nominal_size)*cladding_hardness))
+    
+    # Overall heat transfer coefficient
+    htc = conduction + radiation #+ contact
 
-    # Set the first and last values explicitly
-    interval[0] = 0
-    interval[-1] = 850
-
-    # Calculate midpoints for each interval
-    for i in range(1, length_h_from_c):
-        interval[i] = (h_from_c[i - 1] + h_from_c[i]) / 2
-
-    # Define a function that has a cosine in each interval with the peak at the midpoint and varying amplitude
-    value = 0
-    for i in range(1, len(interval)):
-        start = interval[i - 1]
-        end = interval[i]
-        midpoint = (start + end) / 2
-        amplitude = peak_factors[i - 1]
-        adjusted_start = start - extrapolated_length
-        adjusted_end = end + extrapolated_length
-        frequency = np.pi / (adjusted_end - adjusted_start)
-        if adjusted_start <= x < adjusted_end:
-            value += amplitude * np.cos(frequency * (x - midpoint))
-    return [value, interval]
-
-# Generate x values and corresponding y values for the plot
-x_values = np.linspace(0, 850, 1000)
-y_values = [power_profile(x)[0] for x in x_values]
-
-# Use spline interpolation to smooth the curve without altering the peaks
-interval = power_profile(x_values[0])[1]
-x_peaks = [(interval[i - 1] + interval[i]) / 2 for i in range(1, len(interval))]
-y_peaks = peak_factors
-spl = make_interp_spline(x_values, y_values, k=3)
-y_values_smooth = spl(x_values)
-
-# Calculate maximum and minimum points
-max_points_x = x_peaks
-max_points_y = peak_factors
-min_points_x = interval
-min_points_y = [0] * len(interval)
-
-# Plotting the graph
-plt.figure(figsize=(10, 6))
-
-# Plot dotted lines at interval start and end
-for point in interval:
-    plt.axvline(x=point, color='r', linestyle='--', linewidth=1)
-
-# Plot dotted lines at midpoints
-for point in h_from_c:
-    plt.axvline(x=point, color='b', linestyle='-.', linewidth=1)
-
-# Plot the smoothed cosine function
-plt.plot(x_values, y_values_smooth, color='g', label='Smoothed Cosine Function in Each Interval with Peak Factors')
-
-# Plot black dashed line connecting the maximum points
-plt.plot(max_points_x, max_points_y, color='k', linestyle='--', linewidth=1.5, label='Max Points Connection')
-
-# Plot black dashed line connecting the minimum points
-plt.plot(min_points_x, min_points_y, color='k', linestyle='--', linewidth=1.5, label='Min Points Connection')
-
-plt.xlabel('Position')
-plt.ylabel('Value')
-plt.title('Interval Start, End, Midpoints, and Smoothed Cosine Function with Peak Factors')
-plt.grid(True)
-plt.legend()
-plt.show()
+    # Calculate the thermal resistance of the gap
+    thermal_resistance = 1/(2 * np.pi * radius_gap_in * htc)
+    return thermal_resistance
--- functions.py
+++ functions.py
@@ -1,13 +1,18 @@
 #include libraries
 import numpy as np
-import matplotlib.pyplot as plt
 
 # Define the class for the Material Properties
 class Material_Proprieties:
-    def __init__(self, Elements='', Qualities='', Density='', Molar_Mass='', Micro_Fission='', Micro_Absorption='', Viscosity='', Thermal_Conductivity='', Specific_Heat='', Thermal_Expansion_Coeff='', Melting_Temperature='', Boiling_Temperature='', Youngs_Modulus='', Poissons_Ratio='', Yield_Stress='', Ultimate_Tensile_Strength='', Nusselt_Number=''):
+    def __init__(self, Elements='', Qualities='', Density='',Theoretical_Density='',Percent_of_Theoretical_Density='', Emissivity ='', Molar_Mass='', Micro_Fission='', Micro_Absorption='', Viscosity='', Thermal_Conductivity='', Specific_Heat='', Thermal_Expansion_Coeff='', Melting_Temperature='', Boiling_Temperature='',Oxigen_to_metal_ratio='', Youngs_Modulus='', Poissons_Ratio='', Yield_Stress='', Ultimate_Tensile_Strength='', Nusselt_Number='', Grain_diameter=''):
         self.Elements = Elements
         self.Qualities = Qualities
-        self.Density = Density
+        self.Theoretical_Density = Theoretical_Density
+        self.Percent_of_Theoretical_Density = Percent_of_Theoretical_Density
+        if Density == '':
+            self.Density = self.Theoretical_Density * self.Percent_of_Theoretical_Density / 100
+            self.Porosity = (self.Theoretical_Density - self.Density) / self.Density
+        else:
+            self.Density = Density
         self.Molar_Mass = Molar_Mass
         self.Micro_Fission = Micro_Fission
         self.Micro_Absorption = Micro_Absorption
@@ -15,47 +20,67 @@ class Material_Proprieties:
         self.Thermal_Conductivity = Thermal_Conductivity
         self.Specific_Heat = Specific_Heat
         self.Thermal_Expansion_Coeff = Thermal_Expansion_Coeff
+        self.Emissivity = Emissivity
         self.Melting_Temperature = Melting_Temperature
         self.Boiling_Temperature = Boiling_Temperature
+        self.Oxigen_to_metal_ratio = Oxigen_to_metal_ratio
         self.Youngs_Modulus = Youngs_Modulus
         self.Poissons_Ratio = Poissons_Ratio
         self.Yield_Stress = Yield_Stress
         self.Ultimate_Tensile_Strength = Ultimate_Tensile_Strength
         self.Nusselt_Number = Nusselt_Number
+        self.Grain_diameter = Grain_diameter
 
+        
+# Geometry Data
 class GeometryData:
-    def __init__(self, fuel_outer_diameter, fuel_inner_diameter, cladding_outer_diameter, thickness_cladding):
-        self.fuel_outer_diameter = fuel_outer_diameter  # mm
-        self.fuel_inner_diameter = fuel_inner_diameter  # mm (0 if solid fuel pellet)
-        self.cladding_outer_diameter = cladding_outer_diameter  # mm
-        self.thickness_cladding = thickness_cladding  # mm
+    def __init__(self, fuel_outer_diameter, fuel_inner_diameter, cladding_outer_diameter, thickness_cladding, pin_pitch, h_values, fuel_pellet_height, fuel_roughness, cladding_roughness):
+        self.fuel_outer_diameter = fuel_outer_diameter  # m
+        self.fuel_inner_diameter = fuel_inner_diameter  # m (0 if solid fuel pellet)
+        self.cladding_outer_diameter = cladding_outer_diameter  # m
+        self.thickness_cladding = thickness_cladding  # m
+        self.pin_pitch = pin_pitch # m
+        self.h_values = h_values  # m
+        self.fuel_pellet_height = fuel_pellet_height  # m
+        self.nominal_size = (cladding_outer_diameter - fuel_outer_diameter)/2 - thickness_cladding
+        self.effective_gap_size = self.nominal_size + fuel_roughness + cladding_roughness
 
-    def get_fuel_radius(self):
-        return self.fuel_outer_diameter / 2
 
-    def get_cladding_radius(self):
-        return self.cladding_outer_diameter / 2
-
-    def get_effective_gap_size(self, fuel_roughness, cladding_roughness):
-        nominal_size = self.cladding_outer_diameter - self.fuel_outer_diameter - self.thickness_cladding
-        return nominal_size + fuel_roughness + cladding_roughness
-
+# Thermo Hydraulics Specs
 class ThermoHydraulicSpecs:
-    def __init__(self, coolant_inlet_temp, coolant_inlet_pressure, coolant_mass_flow_rate, pin_pitch):
+    def __init__(self, coolant_inlet_temp, coolant_inlet_pressure, coolant_mass_flow_rate, q_linear_avg, neutron_flux_peak):
         self.coolant_inlet_temp = coolant_inlet_temp  # K
-        self.coolant_inlet_pressure = coolant_inlet_pressure  # MPa
+        self.coolant_inlet_pressure = coolant_inlet_pressure  # Pa
         self.coolant_mass_flow_rate = coolant_mass_flow_rate  # kg/s
-        self.pin_pitch = pin_pitch  # mm
+        self.q_linear_avg = q_linear_avg  # W/m
+        self.neutron_flux_peak = neutron_flux_peak  # kg/s
 
+# Define temperature map
+class Temperature_Map:
+    def __init__(self, X, Y, Z):
+        self.r = X
+        self.h = Y
+        self.T = Z
+        
+# Define the class for Dimensioning Data
+class DimensioningData:
+    def __init__(self, filling_gas_pressure, filling_gas_temperature, temperature_map):
+        self.p_gas = filling_gas_pressure  # Pa
+        self.T_gas = filling_gas_temperature  # °C
+        self.T_map = temperature_map
+
+##################################################
+# Hydraulic Flow
+##################################################
 # Function to calculate the hydraulic flow parameters
 def hydraulic_flow(thermo_hyd_spec, geom_data, coolant, temperature):
-    passage_area = (1/2 * thermo_hyd_spec.pin_pitch * thermo_hyd_spec.pin_pitch * np.sin(np.pi/3)) - (1/2 * np.pi * (geom_data.get_cladding_radius() * 1e-3)**2)
+    passage_area = (1/2 * (geom_data.pin_pitch ** 2) * np.sin(np.pi/3)) - (1/2 * np.pi * (geom_data.cladding_outer_diameter/2)**2)
 
     # Calculate the velocity of the fluid
     velocity = thermo_hyd_spec.coolant_mass_flow_rate / (coolant.Density(temperature) * passage_area)
 
     # Calculate the hydraulic diameter
-    wetted_perimeter = np.pi * geom_data.get_cladding_radius() * 1e-3
+    wetted_perimeter = np.pi * geom_data.cladding_outer_diameter / 2
 
     hydraulic_diameter = (4 * passage_area) / (wetted_perimeter)
 
@@ -102,25 +127,25 @@ def power_profile(h, h_from_c, peak_factors, q_linear_avg):
 # Thermal Resistances
 ##################################################
 def thermal_resistance_coolant(geom_data, coolant_htc):
-    radius_cladding_out = geom_data.get_cladding_radius() * 1e-3  # Convert to meters
+    radius_cladding_out = geom_data.cladding_outer_diameter / 2
     thermal_resistance = 1 / (2 * np.pi * radius_cladding_out * coolant_htc)
     return thermal_resistance
 
 def thermal_resistance_cladding(geom_data, cladding, temperature):
-    radius_cladding_out = geom_data.get_cladding_radius() * 1e-3
-    radius_cladding_in = (geom_data.fuel_outer_diameter / 2) * 1e-3  # Convert to meters
+    radius_cladding_out = geom_data.cladding_outer_diameter / 2
+    radius_cladding_in = radius_cladding_out - geom_data.thickness_cladding
     k = cladding.Thermal_Conductivity(temperature)
     thermal_resistance = np.log(radius_cladding_out / radius_cladding_in) / (2 * np.pi * k)
     return thermal_resistance
 
-def thermal_resistance_gap(geom_data, effective_size, helium, fuel, cladding, temperature_gas, temperature_fuel_out, temperature_cladding_in, contact_pressure, cladding_hardness, C, emissivity_fuel, emissivity_cladding):
-    radius_gap_in = geom_data.get_fuel_radius() * 1e-3  # Convert to meters
+def thermal_resistance_gap(geom_data, helium, fuel, cladding, temperature_gas, temperature_fuel_out, temperature_cladding_in, contact_pressure, cladding_hardness, C):
+    radius_gap_in = geom_data.fuel_outer_diameter / 2
 
     # Conductive heat transfer
-    conduction = helium.Thermal_Conductivity(temperature_gas) / effective_size
+    conduction = helium.Thermal_Conductivity(temperature_gas) / geom_data.effective_gap_size
 
     # Radiative heat transfer
-    radiation = 4 * 5.67e-8 * (temperature_fuel_out**3) / (1/(emissivity_fuel) + 1/(emissivity_cladding) - 1)
+    radiation = 4 * 5.67e-8 * (temperature_fuel_out**3) / (1/(fuel.Emissivity) + 1/(cladding.Emissivity) - 1)
 
     # Overall heat transfer coefficient
     htc = conduction + radiation
@@ -129,14 +154,14 @@ def thermal_resistance_gap(geom_data, effective_size, helium, fuel, cladding, te
     thermal_resistance = 1 / (2 * np.pi * radius_gap_in * htc)
     return thermal_resistance
 
-def thermal_resistance_fuel(Burnup, temperature, Oxigen_to_metal_ratio, Pu_concentration, porosity):
-    A = 0.01926 + 1.06e-6 * Oxigen_to_metal_ratio + 2.63e-8 * Pu_concentration
-    B = 2.39e-4 + 1.37e-13 * Pu_concentration
+def thermal_resistance_fuel(Burnup, temperature, fuel):
+    A = 0.01926 + 1.06e-6 * fuel.Oxigen_to_metal_ratio + 2.63e-8 * fuel.Molar_Mass[-1]
+    B = 2.39e-4 + 1.37e-13 * fuel.Molar_Mass[-1]
     D = 5.27e9
     E = 17109.5
 
     # Calculate k_0
-    k_0 = (1 / (A + B * temperature) + D / (temperature**2) * np.exp(-E / temperature)) * (1 - porosity)**2.5
+    k_0 = (1 / (A + B * temperature) + D / (temperature**2) * np.exp(-E / temperature)) * (1 - fuel.Porosity)**2.5
 
     # Calculate final thermal conductivity k
     k = 1.755 + (k_0 - 1.755) * np.exp(-Burnup / 128.75)
@@ -148,67 +173,102 @@ def thermal_resistance_fuel(Burnup, temperature, Oxigen_to_metal_ratio, Pu_conce
 ##################################################
 # Temperature Profiles
 ##################################################
-def axial_T_profile_coolant(thermo_hyd_spec, power, h, dz, coolant, heights_of_slice_centre, peak_factors, q_linear_avg):
+def axial_T_profile_coolant(Temp_old, thermo_hyd_spec, power, h, dz, coolant, heights_of_slice_centre, peak_factors, q_linear_avg):
     f = 1/2
-    c_p = coolant.Specific_Heat(thermo_hyd_spec.coolant_inlet_temp)
+    c_p = coolant.Specific_Heat(Temp_old)
+    m_rate = thermo_hyd_spec.coolant_mass_flow_rate
 
     power = power_profile(h, heights_of_slice_centre, peak_factors, q_linear_avg)
 
-    T_now = thermo_hyd_spec.coolant_inlet_temp + power * dz / (thermo_hyd_spec.coolant_mass_flow_rate / f * c_p)
-
+    T_now = Temp_old + power * dz / ( m_rate / f * c_p)
     return T_now
-  
-def temperature_profile(power, thermal_resistance, T_init):
-    return T_init + power * thermal_resistance
 
-def get_resistance(r, geom_data, Resistances):
-    if r < geom_data.fuel_outer_diameter:
-        if geom_data.fuel_inner_diameter == 0:
-            res = Resistances.Fuel * (-r**2)
-        else:
-            res = Resistances.Fuel * np.log(geom_data.fuel_outer_diameter*1e-3 / 2 / r) / np.log(geom_data.fuel_inner_diameter / geom_data.fuel_outer_diameter)
-        return res
-    elif r < geom_data.fuel_outer_diameter + geom_data.thickness_cladding:
-        return Resistances.Gap
-    elif r < geom_data.cladding_outer_diameter:
-        return Resistances.Cladding
-    else:
-        return Resistances.Coolant
-
-def compute_temperature_profile(Temp_0, reference_power_density, r_plot, geom_data, Resistances):
-    T_plot = [Temp_0]
+def radial_temperature_profile(Temp_0, power, r_plot, geom_data, Resistances, fuel_data, Burnup):
+    # Initialize the temperature profile
+    T_radial = [Temp_0]
+    
+    # Find the index of the gap outer diameter == cladding inner diameter
+    idx_gap_r = np.argmin(np.abs(r_plot - geom_data.cladding_outer_diameter/2 - geom_data.thickness_cladding))
+    
+    # Find the index of the fuel outer diameter
+    idx_fuel_r = np.argmin(np.abs(r_plot - geom_data.fuel_outer_diameter/2))
+    
     for j, r in enumerate(r_plot[1:], start=1):
-        R = get_resistance(r, geom_data, Resistances)
-        T_plot.append(temperature_profile(reference_power_density, R, T_plot[j-1]))
-    return T_plot
+        # Get the thermal resistance
+        dr = r_plot[j] - r_plot[j-1]
+        
+        # In the pellet
+        if r < geom_data.fuel_outer_diameter/2:
+            th_res = thermal_resistance_fuel(Burnup, T_radial[j-1], fuel_data)
+            # Solid fuel pellet
+            if geom_data.fuel_inner_diameter == 0: 
+                r_out = geom_data.fuel_outer_diameter / 2
+                res = th_res * (1 - (r/r_out)**2)
+                
+            # Hollow fuel pellet
+            else: 
+                r_out = geom_data.fuel_outer_diameter / 2
+                r_in = geom_data.fuel_inner_diameter / 2 
+                
+                res = th_res * np.log(r_out/r) / np.log(r_in/r_out)
+            
+            # Compute the temperature
+            T_value = T_radial[j-1] + power * res * r
+            
+        # In the gap
+        elif r < geom_data.cladding_outer_diameter / 2 - geom_data.thickness_cladding:
+            r_out = geom_data.cladding_outer_diameter / 2 - geom_data.thickness_cladding
+            r_in = geom_data.fuel_outer_diameter / 2
+            res = Resistances.Gap * np.log(r_out/r) / np.log(r_in/r_out) 
+            
+            # Compute the temperature
+            T_value = T_radial[j-1] + power * res * r
+            
+        # In the cladding
+        elif r < geom_data.cladding_outer_diameter/2:
+            res = Resistances.Cladding
+            T_value = T_radial[j-1] + power * res
+            
+        # In the coolant
+        else:
+            res = Resistances.Coolant
+            T_value = T_radial[j-1] + power * res
 
-def plot_3d_temperature_profile(h_values, r_plot, heights_of_slice_centre, peak_factors, q_linear_avg, Resistances, coolant, thermo_hyd_spec, geom_data):
-    fig = plt.figure(figsize=(12, 8))
-    ax = fig.add_subplot(111, projection='3d')
+        # Compute new value of T
+        T_radial.append(T_value)
+        
+    return T_radial
 
+def temperature_profile_3D(r_values, heights_of_slice_centre, peak_factors, Resistances, coolant, thermo_hyd_spec, geom_data, fuel_data, Burnup):
+    h_values = geom_data.h_values
+
+    # Compute the height of each slice
     dz = h_values[1] - h_values[0]
+    
 
-    X, Y = np.meshgrid(r_plot, h_values)
+    # Create the meshgrid
+    X, Y = np.meshgrid(r_values, h_values)
     Z = []
+    
+    # Initialize the temperature
+    Temp_coolant = thermo_hyd_spec.coolant_inlet_temp
 
-    Temp_0 = thermo_hyd_spec.coolant_inlet_temp
-    for h in h_values:       
-        q = power_profile(h, heights_of_slice_centre, peak_factors, q_linear_avg)
-        Temp_0 = axial_T_profile_coolant(thermo_hyd_spec, q, h, dz, coolant, heights_of_slice_centre, peak_factors, q_linear_avg)
-        T_plot = compute_temperature_profile(Temp_0, q, r_plot, geom_data, Resistances)
+    # Compute the temperature profile for each height step
+    for h in h_values: 
+        q = power_profile(h, heights_of_slice_centre, peak_factors, thermo_hyd_spec.q_linear_avg)
+        # Compute temperature profile
+        Temp_coolant = axial_T_profile_coolant(Temp_coolant, thermo_hyd_spec, q, h, dz, coolant, heights_of_slice_centre, peak_factors, thermo_hyd_spec.q_linear_avg)
+        T_plot = radial_temperature_profile(Temp_coolant, q, r_values, geom_data, Resistances, fuel_data, Burnup)
         Z.append(T_plot)
 
     Z = np.array(Z)
-    ax.plot_surface(X, Y, Z, cmap='viridis')
-    ax.set_xlabel('Radius (mm)')
-    ax.set_ylabel('Height (mm)')
-    ax.set_zlabel('Temperature (K)')
-    ax.set_title("3D Temperature Profile vs. Radius and Height (q_values)")
-    plt.show()
+    T_map = Temperature_Map(X, Y, Z)
+    return T_map
 
-    return X, Y, Z
-
-def get_temperature_at_point(h, r, X, Y, Z):
-    h_idx = np.argmin(np.abs(Y[:, 0] - h))
-    r_idx = np.argmin(np.abs(X[0, :] - r))
-    return Z[h_idx, r_idx]
+def get_temperature_at_point(h_requested, r_requested,T_map):
+    h_values = T_map.h
+    r_values = T_map.r
+    T_values = T_map.T
+    h_idx = np.argmin(np.abs(h_values[:, 0] - h_requested))
+    r_idx = np.argmin(np.abs(r_values[0, :] - r_requested))
+    return T_values[h_idx, r_idx]
