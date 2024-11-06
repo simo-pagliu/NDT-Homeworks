062f9bd28ae500ddd4e0869491ed9e13f8fb159e
leesarr <160870997+leesarr@users.noreply.github.com>
2024-11-06 16:16:39 +0100

    Central void
    Pellet regions

--- another_try.py
+++ another_try.py
@@ -5,43 +5,54 @@ import scipy.optimize as opt
 import sympy as sp
 import functions as f
 
-def radius_finder(guess, beta, R_init, linear_heat_rate, Fuel_Properties, vars):
+# def radius_finder(guess, beta, R_init, linear_heat_rate, Fuel_Properties, vars):
 
-    h = 425 #mm
-    T_R_toget = guess
-    T_f_outer = f.get_temperature_at_point(h,R_init,vars.T_map)
+#     h = 425 #mm
+#     T_R_toget = guess
+#     T_f_outer = f.get_temperature_at_point(h,R_init,vars.T_map)
 
-    thermal_resistance = f.thermal_resistance_fuel(beta, Fuel_Properties)(T_f_outer)
+#     thermal_resistance = f.thermal_resistance_fuel(beta, Fuel_Properties)(T_f_outer)
 
-    deltaT = linear_heat_rate/thermal_resistance
+#     deltaT = linear_heat_rate/thermal_resistance
 
-    R_toget = np.sqrt(1-(T_R_toget - T_f_outer)/deltaT)*R_init
+#     R_toget = np.sqrt(1-(T_R_toget - T_f_outer)/deltaT)*R_init
 
-    return R_toget, T_R_toget 
+#     return R_toget, T_R_toget 
+##################################################
+# Central Void Formation
+##################################################
+# def get_radius_at_temprature(T_requested,T_map):
 
-def R_void(Geometrical_Data, Fuel_Properties, R_col, R_eq):
+#     T_values = T_map.T
+#     T_idx = np.argmin(np.abs(T_values - T_requested))
+#     print(T_idx)
+#     return T_idx
+
+# xxx = get_radius_at_temprature(1800, T_map)
+
+#######
+def get_radius_at_temprature(T_requested,T_map):
+
+    r_values = []
+    T_values = T_map.T
+    for t,r,h in zip(T_values,T_map.r,T_map.h):
+        T_idx = np.argmin(np.abs(t - T_requested))
+        r_values.append(r[T_idx])
     
-    # from mass conservation equation using sympy
-    R_void = sp.symbols('R_void')
-    R_f_outer = sp.symbols('R_f_outer')
-    R_columnar = sp.symbols('R_columnar')
-    R_equiaxed = sp.symbols('R_equiaxed')
-    density_af = sp.symbols('density_af')
-    density_columnar = sp.symbols('density_columnar')
-    density_equiaxed = sp.symbols('density_equiaxed')
+    return r_values
 
-    #write the equation LHS 
-    LHS = R_f_outer**2*density_af
-    RHS = (R_columnar**2-R_void**2)*density_columnar + (R_equiaxed**2-R_columnar**2)*density_equiaxed + (R_f_outer**2-R_equiaxed**2)*density_af
-
-    # substitute the values of the variables
-    LHS_sub = LHS.subs({R_f_outer: Geometrical_Data.fuel_outer_diameter/2, density_af: Fuel_Properties.Density})
-    RHS_sub = RHS.subs({R_columnar: R_col, R_equiaxed: R_eq, density_columnar: Fuel_Properties.Theoretical_Density*0.98, density_equiaxed: Fuel_Properties.Theoretical_Density*0.95, R_f_outer: Geometrical_Data.fuel_outer_diameter/2, density_af: Fuel_Properties.Density})
-
-    #solve the equation for R_void
-    eq = sp.Eq(LHS_sub, RHS_sub)
-    R_void = sp.solve(eq, R_void)[1]
-
+def get_R_void(Fuel_Properties, R_col, R_eq):
+        
+    R_void = []
+    density_af = Fuel_Properties.Density
+    density_columnar=Fuel_Properties.Theoretical_Density*0.98
+    density_equiaxed= Fuel_Properties.Theoretical_Density*0.95
+    
+    for r in range(len(R_col)):
+        
+        R_voidd = np.sqrt(R_col[r]**2 - R_eq[r]**2*(density_af/density_columnar) +(R_eq[r]**2 - R_col[r]**2)*(density_equiaxed/density_af))
+        R_void.append(R_voidd)
+        
     return R_void
 
     
